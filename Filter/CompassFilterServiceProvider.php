e66147b2dbb19cfcde8f2ea61285dcfa772f4f8a
Benjamin Doherty <bjd@bangpound.org>
2014-10-01 06:23:18 -0500

    unwrap false null and true.

--- Filter/CompassFilterServiceProvider.php
+++ Filter/CompassFilterServiceProvider.php
@@ -21,8 +21,8 @@ class CompassFilterServiceProvider implements ServiceProviderInterface
         $pimple['assetic.filter.compass.class'] = 'Assetic\Filter\CompassFilter';
         $pimple['assetic.filter.compass.bin'] = '/usr/bin/compass';
         $pimple['assetic.filter.compass.timeout'] = null;
-        $pimple['assetic.filter.compass.debug'] = 'false';
-        $pimple['assetic.filter.compass.no_line_comments'] = 'false';
+        $pimple['assetic.filter.compass.debug'] = false;
+        $pimple['assetic.filter.compass.no_line_comments'] = false;
         $pimple['assetic.filter.compass.style'] = null;
         $pimple['assetic.filter.compass.images_dir'] = null;
         $pimple['assetic.filter.compass.fonts_dir'] = null;
@@ -35,7 +35,7 @@ class CompassFilterServiceProvider implements ServiceProviderInterface
         $pimple['assetic.filter.compass.http_javascripts_path'] = null;
         $pimple['assetic.filter.compass.plugins'] = array();
         $pimple['assetic.filter.compass.load_paths'] = array();
-        $pimple['assetic.filter.compass.home_env'] = 'true';
+        $pimple['assetic.filter.compass.home_env'] = true;
 
         $pimple['assetic.filter.compass'] = function (Container $c) {
             /** @var \Assetic\Filter\CompassFilter $filter */
